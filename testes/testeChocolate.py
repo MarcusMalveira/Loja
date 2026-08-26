9754b71eeb26588bcc5e8b856726567d8d5344fc
MarcusMalveira <vinimalver410@gmail.com>
2026-08-26 08:32:59 -0500

    Remove caminho local dos testes Selenium

--- testes/testeChocolate.py
+++ testes/testeChocolate.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import Select
 import time
 
 driver = webdriver.Chrome()
-driver.get("http://localhost/Marcus/Exercicios/banco/loja.php")
+driver.get("[coloque o caminho do arquivo aqui]")
 
 select_element = driver.find_element(By.ID, "produto")
 
