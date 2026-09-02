d4057b08d848fc116d69920a67afcee7751ea2ae
MarcusMalveira <vinimalver410@gmail.com>
2026-09-02 09:29:59 -0500

    Troca seletores do teste de compra por IDs

--- testes/testeComprando.py
+++ testes/testeComprando.py
@@ -4,84 +4,45 @@ from selenium.webdriver.support.ui import Select
 import time
 
 driver = webdriver.Chrome()
-
 driver.get("http://localhost/Marcus/Exercicios/banco/loja.php")
 
 # Seleciona Milho
-select_element = driver.find_element(By.ID, "produto")
-select_produto = Select(select_element)
-
+select_produto = Select(driver.find_element(By.ID, "produto"))
 select_produto.select_by_visible_text("milho")
-
 time.sleep(2)
 
 # Coloca 20 unidades
 campo_quantidade = driver.find_element(By.ID, "quantidade")
 campo_quantidade.send_keys("20")
-
 time.sleep(2)
 
 # Adiciona ao carrinho
-botao_carrinho = driver.find_element(
-    By.CSS_SELECTOR,
-    "button[type='submit']"
-)
-
-botao_carrinho.click()
-
+driver.find_element(By.ID, "adicionar_carrinho").click()
 time.sleep(3)
 
 # Limpa o carrinho
-botao_limpar = driver.find_element(
-    By.XPATH,
-    "//button[text()='Limpar Carrinho']"
-)
-
-botao_limpar.click()
-
+driver.find_element(By.ID, "limpar_carrinho").click()
 time.sleep(3)
 
 # Volta para a loja
-link_voltar = driver.find_element(
-    By.LINK_TEXT,
-    "Voltar para a loja"
-)
-
-link_voltar.click()
-
+driver.find_element(By.ID, "voltar_loja").click()
 time.sleep(3)
 
 # Seleciona Milho novamente
-select_element = driver.find_element(By.ID, "produto")
-select_produto = Select(select_element)
-
+select_produto = Select(driver.find_element(By.ID, "produto"))
 select_produto.select_by_visible_text("milho")
-
 time.sleep(2)
 
 # Adiciona agora apenas 15 unidades
 campo_quantidade = driver.find_element(By.ID, "quantidade")
 campo_quantidade.send_keys("15")
-
 time.sleep(2)
 
-botao_carrinho = driver.find_element(
-    By.CSS_SELECTOR,
-    "button[type='submit']"
-)
-
-botao_carrinho.click()
-
+driver.find_element(By.ID, "adicionar_carrinho").click()
 time.sleep(3)
 
 # Finaliza a compra
-botao_finalizar = driver.find_element(
-    By.XPATH,
-    "//button[text()='Finalizar Compra']"
-)
-
-botao_finalizar.click()
-
+driver.find_element(By.ID, "finalizar_compra").click()
 time.sleep(5)
 
 driver.quit()
