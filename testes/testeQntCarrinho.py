6b8d59e27527cb7eaf31832ab834aa31c1c7c21b
MarcusMalveira <vinimalver410@gmail.com>
2026-09-02 09:29:49 -0500

    Padroniza teste de quantidade usando IDs

--- testes/testeQntCarrinho.py
+++ testes/testeQntCarrinho.py
@@ -4,24 +4,20 @@ from selenium.webdriver.support.ui import Select
 import time
 
 driver = webdriver.Chrome()
-
 driver.get("http://localhost/Marcus/Exercicios/banco/loja.php")
 
 # Seleciona milho
 select_produto = Select(driver.find_element(By.ID, "produto"))
 select_produto.select_by_visible_text("milho")
-
 time.sleep(1)
 
 # Adiciona 20 unidades
 quantidade = driver.find_element(By.ID, "quantidade")
 quantidade.send_keys("20")
-
 time.sleep(1)
 
 # Adiciona ao carrinho
-driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-
+driver.find_element(By.ID, "adicionar_carrinho").click()
 time.sleep(2)
 
 # Diminui de 20 para 12
@@ -29,11 +25,9 @@ for i in range(8):
     driver.find_element(By.ID, "diminuir").click()
     time.sleep(0.5)
 
-time.sleep(1)
-
 # Finaliza a compra
+time.sleep(1)
 driver.find_element(By.ID, "finalizar_compra").click()
-
 time.sleep(3)
 
 driver.quit()
