3344708b07885dc8e0d76021177bd869b24572f0
MarcusMalveira <vinimalver410@gmail.com>
2026-09-02 09:30:17 -0500

    Troca seletores de cadastro por IDs

--- testes/testeCadastro.py
+++ testes/testeCadastro.py
@@ -1,36 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 import time
 
 driver = webdriver.Chrome()
-
 driver.get("http://localhost/Marcus/Exercicios/banco/loja.php")
-
 time.sleep(2)
 
-link_adicionar = driver.find_element(By.ID, "adicionar")
-
-link_adicionar.click()
-
+driver.find_element(By.ID, "adicionar").click()
 time.sleep(2)
 
-campo_nome = driver.find_element(By.NAME, "nome")
-campo_nome.send_keys("Feijão")
+driver.find_element(By.ID, "nome").send_keys("Feijão")
+driver.find_element(By.ID, "valor").send_keys("30.00")
+driver.find_element(By.ID, "quantidade").send_keys("300")
 
-campo_valor = driver.find_element(By.NAME, "valor")
-campo_valor.send_keys("30.00")
-
-campo_quantidade = driver.find_element(By.NAME, "quantidade")
-campo_quantidade.send_keys("300")
-
-campo_validade = driver.find_element(By.NAME, "validade")
-campo_validade.send_keys("25082026")
-
+campo_validade = driver.find_element(By.ID, "validade")
+driver.execute_script("arguments[0].value = '2026-08-25';", campo_validade)
 time.sleep(2)
 
-botao_cadastrar = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-botao_cadastrar.click()
+driver.find_element(By.ID, "cadastrar").click()
 time.sleep(3)
 
 driver.quit()
