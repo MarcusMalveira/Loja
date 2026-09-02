9601174c570d1f29ca2a7ff78a39d5ebe156023a
MarcusMalveira <vinimalver410@gmail.com>
2026-09-02 09:30:08 -0500

    Usa IDs no teste de adicionar ao carrinho

--- testes/testeAddCarrinho.py
+++ testes/testeAddCarrinho.py
@@ -6,82 +6,38 @@ import time
 driver = webdriver.Chrome()
 driver.get("http://localhost/Marcus/Exercicios/banco/loja.php")
 
-# Seleciona o elemento select pelo ID
-select_element = driver.find_element(By.ID, "produto")
-
-#transforma em um select controlavel
-select_produto = Select(select_element)
-
-#seleciona o produto "Chocolate" pelo texto visível
+# Adiciona Chocolate
+select_produto = Select(driver.find_element(By.ID, "produto"))
 select_produto.select_by_visible_text("Chocolate")
-
 time.sleep(2)
 
-#acha o campo quantidade
-campo_quantidade = driver.find_element(By.ID, "quantidade")
-
-#digita a quantidade desejada
-campo_quantidade.send_keys("2")
-
+driver.find_element(By.ID, "quantidade").send_keys("2")
 time.sleep(2)
-
-#acha o botão de adicionar ao carrinho
-botao_carrinho = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-
-botao_carrinho.click()
+driver.find_element(By.ID, "adicionar_carrinho").click()
 time.sleep(3)
 
-link_voltar = driver.find_element(By.LINK_TEXT, "Continuar comprando")
-link_voltar.click()
-
-# Seleciona o elemento select pelo ID
-select_element = driver.find_element(By.ID, "produto")
-
-#transforma em um select controlavel
-select_produto = Select(select_element)
-
-#seleciona o produto "Chocolate" pelo texto visível
-select_produto.select_by_visible_text("Carne")
-
+driver.find_element(By.ID, "continuar_comprando").click()
 time.sleep(2)
 
-#acha o campo quantidade
-campo_quantidade = driver.find_element(By.ID, "quantidade")
+# Adiciona Carne
+select_produto = Select(driver.find_element(By.ID, "produto"))
+select_produto.select_by_visible_text("Carne")
+time.sleep(2)
 
-#digita a quantidade desejada
-campo_quantidade.send_keys("2")
-
-
-botao_carrinho = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-botao_carrinho.click()
-
+driver.find_element(By.ID, "quantidade").send_keys("2")
+driver.find_element(By.ID, "adicionar_carrinho").click()
 time.sleep(3)
 
-link_voltar = driver.find_element(By.LINK_TEXT, "Continuar comprando")
-link_voltar.click()
-
+driver.find_element(By.ID, "continuar_comprando").click()
 time.sleep(2)
 
-# Seleciona o elemento select pelo ID
-select_element = driver.find_element(By.ID, "produto")
-
-#transforma em um select controlavel
-select_produto = Select(select_element)
-
-#seleciona o produto "Chocolate" pelo texto visível
+# Adiciona Carne novamente
+select_produto = Select(driver.find_element(By.ID, "produto"))
 select_produto.select_by_visible_text("Carne")
-
 time.sleep(2)
 
-#acha o campo quantidade
-campo_quantidade = driver.find_element(By.ID, "quantidade")
-
-#digita a quantidade desejada
-campo_quantidade.send_keys("2")
-
-botao_carrinho = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-botao_carrinho.click()
-
+driver.find_element(By.ID, "quantidade").send_keys("2")
+driver.find_element(By.ID, "adicionar_carrinho").click()
 time.sleep(3)
 
 driver.quit()
